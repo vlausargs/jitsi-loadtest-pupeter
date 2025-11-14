55ad8e291758f07862b441aed300e8850240cce3
vlausargs <vlaus.mi@gmail.com>
2025-11-15 01:08:40 +0700

    up

--- run20.sh
+++ run20.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 screen -S bbbload -dm bash -lc '
   cd ~/workspace/jitsi-loadtest-pupeter/
-  TOTAL_USERS=20 CONCURRENCY=20 VIDEO_ENABLE=true AUDIO_ENABLE=true node index1.js
+  TOTAL_USERS=20 CONCURRENCY=20 VIDEO_ENABLE=true AUDIO_ENABLE=true node index.js
 '
