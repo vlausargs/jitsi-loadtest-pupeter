927a9ebd5cc2cd5c9ecaa143b62d4f9906b51956
vlausargs <vlaus.mi@gmail.com>
2025-11-17 14:42:03 +0700

    up

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 config=""
 #001 021 041 061 081 101 121 141 161 181 201
-for i in 001 021 041 061 081 101 121; do
+for i in 001 021 041 061 081 101 121 141 161 181 201; do
     config="$config -f docker-compose.$i.yaml"
 done
 docker compose $config up -d $@ --build --force-recreate
