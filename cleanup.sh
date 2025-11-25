3a17d6986d24b2a4e798724698ef88e78b9abf9d
valospc1 <valospc1@valos.id>
2025-11-25 16:52:49 +0700

    up

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-NAME="bbbload"
-for id in $(screen -ls | awk -v n="$NAME" '$0 ~ n {print $1}'); do
-  screen -S "$id" -X quit
-  sleep 0.5
-  screen -S "$id" -X quit
-done
+# NAME="bbbload"
+# for id in $(screen -ls | awk -v n="$NAME" '$0 ~ n {print $1}'); do
+#   screen -S "$id" -X quit
+#   sleep 0.5
+#   screen -S "$id" -X quit
+# done
 pkill -TERM chrome || true
 
 pkill -9 -f 'node .*index\.js'
+
+exit 0
